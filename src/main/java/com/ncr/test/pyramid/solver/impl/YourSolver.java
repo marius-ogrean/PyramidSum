6d4153828566dacb8b245425b2b991d8bca75a8e
Marius <marius_ogrean@yahoo.com>
2025-08-06 15:47:47 +0300

    add comments about the algorithm

--- src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
@@ -10,6 +10,10 @@ import com.ncr.test.pyramid.solver.PyramidSolver;
 public class YourSolver implements PyramidSolver {
     @Override
     public long pyramidMaximumTotal(Pyramid pyramid) {
+        //we alter the data in the pyramid by always adding the maximum of the values below with which can be a valid source
+        // thus, after processing all the rows we will get the maximum for each cell of the top row
+        // then we get this maximum and return it
+
         for (int i = pyramid.getRows() - 2; i >= 0; i--) {
             for (int j = 0; j < pyramid.getRows() - i; j++) {
                 if (j == 0) {
