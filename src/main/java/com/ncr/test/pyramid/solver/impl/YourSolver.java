0f77ead85c234a5d0ef54dbf5a757455ba891a3b
Marius <marius_ogrean@yahoo.com>
2025-08-06 15:22:23 +0300

    make the algorithm not recursive, thus we ensure it will eventually end....

--- src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
@@ -3,32 +3,50 @@ package com.ncr.test.pyramid.solver.impl;
 import com.ncr.test.pyramid.data.Pyramid;
 import com.ncr.test.pyramid.solver.PyramidSolver;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * TASK: This is your 3rd task.
  * Please implement the class to satisfy the interface. *
  */
 public class YourSolver implements PyramidSolver {
-    private long max = -1;
-
     @Override
     public long pyramidMaximumTotal(Pyramid pyramid) {
-        getTotalAbove(pyramid.getRows() - 1, 0, pyramid, 0);
+        Deque<Node> stack = new ArrayDeque<>();
+
+        stack.push(new Node(pyramid.getRows() - 1, 0, 0));
+
+        long max = -1;
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+
+            int myValue = pyramid.get(current.row, current.column);
+            long newSum = current.sumUpToThisPoint + myValue;
+
+            if (current.row == 0) {
+                if (newSum > max) {
+                    max = newSum;
+                }
+            } else {
+                stack.push(new Node(current.row - 1, current.column + 1, newSum));
+                stack.push(new Node(current.row - 1, current.column, newSum));
+            }
+        }
+
         return max;
     }
 
-    private void getTotalAbove(int row, int column, Pyramid pyramid, long sumUpToThisPoint) {
-        int myValue = pyramid.get(row, column);
-        long newSum = sumUpToThisPoint + myValue;
+    private class Node {
+        public int row;
+        public int column;
+        public long sumUpToThisPoint;
 
-        if (row == 0) {
-            if (newSum > max) {
-                max = newSum;
-            }
-
-            return;
+        public Node(int row, int column, long sumUpToThisPoint) {
+            this.row = row;
+            this.column = column;
+            this.sumUpToThisPoint = sumUpToThisPoint;
         }
-
-        getTotalAbove(row - 1, column, pyramid, newSum);
-        getTotalAbove(row - 1, column + 1, pyramid, newSum);
     }
 }
