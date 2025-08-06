7fd027f3c8dccd60a4a6c2d075f9818fa44df825
Marius <marius_ogrean@yahoo.com>
2025-08-06 15:00:23 +0300

    add another recursive solution which is similarly bad...

--- src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/YourSolver.java
@@ -8,10 +8,27 @@ import com.ncr.test.pyramid.solver.PyramidSolver;
  * Please implement the class to satisfy the interface. *
  */
 public class YourSolver implements PyramidSolver {
+    private long max = -1;
 
     @Override
     public long pyramidMaximumTotal(Pyramid pyramid) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        getTotalAbove(pyramid.getRows() - 1, 0, pyramid, 0);
+        return max;
     }
-    
+
+    private void getTotalAbove(int row, int column, Pyramid pyramid, long sumUpToThisPoint) {
+        int myValue = pyramid.get(row, column);
+        long newSum = sumUpToThisPoint + myValue;
+
+        if (row == 0) {
+            if (newSum > max) {
+                max = newSum;
+            }
+
+            return;
+        }
+
+        getTotalAbove(row - 1, column, pyramid, newSum);
+        getTotalAbove(row - 1, column + 1, pyramid, newSum);
+    }
 }
